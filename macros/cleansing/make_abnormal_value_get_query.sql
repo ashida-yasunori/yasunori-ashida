4b9f5e76a86ca64056d5bc8fa0091e203a48ba4b
孝 親富祖 <t.oyafuso@methodologic.co.jp>
2024-03-01 10:19:22 +0000

    make_abnormal_value_get_query.sql make_abnormal_value_update_query.sql 両方のcreated_atをRAWテーブルのcreated_atの時間にする

--- macros/cleansing/make_abnormal_value_get_query.sql
+++ macros/cleansing/make_abnormal_value_get_query.sql
@@ -6,7 +6,7 @@
 {%- for trend_point in trend_points -%}
 (
 select
-    t1.store_id, t1.log_id, t1.item_index, t1.unit, t1.aggregation_mode, t1.value, t1.value_at, to_timestamp(CURRENT_TIMESTAMP()) as created_at
+    t1.store_id, t1.log_id, t1.item_index, t1.unit, t1.aggregation_mode, t1.value, t1.value_at, t1.created_at
 from {{ source('raw' ~ db_suffix, 'tbl_raw_' ~ trend_point[0]) }} t1
 {{ make_inner_join_sync_log(0) -}}
 inner join {{ source('common' ~ db_suffix, 'tbl_mst_trend_group_point') }} t3 on (
--- macros/cleansing/make_abnormal_value_update_query.sql
+++ macros/cleansing/make_abnormal_value_update_query.sql
@@ -4,7 +4,8 @@
 {% set query %}
 
 select
-	t1.store_id, t1.log_id, t1.item_index, t1.unit, t1.aggregation_mode, null as value, t1.value_at, to_timestamp(CURRENT_TIMESTAMP()) as created_at
+	t1.store_id as store_id, t1.log_id as log_id, t1.item_index as item_index, t1.unit as unit, t1.aggregation_mode as aggregation_mode, null as value, t1.value_at as value_at, t1.created_at
+    -- t1.store_id, t1.log_id, t1.item_index, t1.unit, t1.aggregation_mode, null as value, t1.value_at
 from {{ ref('tbl_mart_abnormal_value') }} t1 
 {{ make_inner_join_sync_log(0) -}}
 inner join {{ source('common' ~ db_suffix, 'tbl_mst_trend_group_point') }} t3 on (
